cfbd70b5220e0dfa83b9d1773125cf39985207f5
Samiul Basir Fahim <93071892+FAHIMekermall@users.noreply.github.com>
2022-04-14 13:32:40 +0600

    blog-details header fixed

--- src/pages/BlogDetails/BlogDetails.js
+++ src/pages/BlogDetails/BlogDetails.js
@@ -1,27 +1,38 @@
+import { Button } from "@mui/material"
 import React, { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 import useBlogs from "../../hooks/useBlogs/useBlogs"
-import './BlogDetails.css'
+import "./BlogDetails.css"
 
 const BlogDetails = () => {
 	const { id } = useParams()
 	const [blogs] = useBlogs()
 	const [post, setPost] = useState({})
+	const navigate = useNavigate()
 	useEffect(() => {
 		if (blogs) {
 			const item = blogs.find((blog) => blog._id === id)
 			setPost(item)
-            console.log(item);
-            console.log(id)
 		}
 	}, [blogs])
 	return (
-		<div className="blog-details-container">
-            <img src={post?.imageURL} alt="" />
-			<h2>{post?.title}</h2>
-            <p>
-                {post?.blog}
-            </p>
+		<div>
+			<div className='blog-details-header'>
+				<Button
+					onClick={() => navigate(-1)}
+					variant="contained"
+					color="info"
+                    style={{height:'100%'}}
+				>
+					Back
+				</Button>
+				<h3>{post?.title?.slice(0, 50)}</h3>
+			</div>
+			<div className="blog-details-container">
+				<img src={post?.imageURL} alt="" />
+				<h2>{post?.title}</h2>
+				<p>{post?.blog}</p>
+			</div>
 		</div>
 	)
 }
--- src/shared/Header/Header.js
+++ src/shared/Header/Header.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link, NavLink } from "react-router-dom"
+import { Link, NavLink, useLocation } from "react-router-dom"
 import "./Header.css"
 import { useAuthState } from "react-firebase-hooks/auth"
 import auth from "../../firebase.init"
@@ -7,7 +7,13 @@ import { Button } from "@mui/material"
 import { signOut } from "firebase/auth"
 
 const Header = () => {
+	const { pathname } = useLocation()
 	const [user] = useAuthState(auth)
+	console.log(pathname)
+	if (pathname.includes("blog")) {
+		return ""
+	}
+
 	return (
 		<header>
 			<nav>
